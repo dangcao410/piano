5da64757bf6c1e471d888fee420d0a8b67c44201
dangcao <dangcao410@gmail.com>
2021-03-01 13:54:31 +0700

    Update audio.

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ let listItems = document.querySelectorAll('.set li');
 listItems.forEach((item, index) => {
     item.addEventListener('click', (event) => {
         const name = item.id;
-        const audio = new Audio(`audio/${name}.mp3`);
+        const audio = new Audio(`audio/piano/${name}.mp3`);
         audio.play();
     });
 });
